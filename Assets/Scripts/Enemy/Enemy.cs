afce9de83ad91e6035b031a84b0cf39239f7eb4b
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-07-22 14:47:54 +0500

    try setting improvement

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,30 +1,91 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent), typeof(Transform))]
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0, 360)] private float _viewAngle = 90f;
+
+    [SerializeField] private float _viewDistance = 15f;
+    [SerializeField] private float _detectionDistance = 3f;
+    [SerializeField] private Transform _player;
+    [Header("")]
     [SerializeField] private Transform[] _goals;
     [SerializeField] private float _distanceToChangeGoal;
-    [SerializeField] private Transform _player;
-    [SerializeField] private float _seeDistance;
     [SerializeField] private LoseCanvas _loseCanvas;
 
+    private Transform _enemyEye;
+    private Transform _agentTransform;
     private NavMeshAgent _agent;
     private int _currentGoal = 0;
+    private float _distanceToPlayer;
+
+    public event Action PlayerDetected;
+    public event Action LostPlayer;
+    public event Action NextGoal;
+
+    private void OnEnable()
+    {
+        PlayerDetected += OnPlayerDetected;
+        LostPlayer += OnPlayerLost;
+        NextGoal += OnNextGoal;
+    }
+
+    private void OnDisable()
+    {
+        PlayerDetected -= OnPlayerDetected;
+        LostPlayer -= OnPlayerLost;
+        NextGoal -= OnNextGoal;
+    }
 
     private void Start()
     {
+        _enemyEye = GetComponent<Transform>();
         _agent = GetComponent<NavMeshAgent>();
 
-        _agent.destination = _goals[0].position;
+        _agentTransform = _agent.transform;
+        
     }
 
     private void Update()
     {
+        _distanceToPlayer = Vector3.Distance(_player.position, _agent.transform.position);
 
-        CheckPlayerDistance();
+        _agent.destination = _goals[0].position;
 
+        if (_agent.remainingDistance < _distanceToChangeGoal)
+        {
+            _currentGoal++;
+
+            if (_currentGoal == _goals.Length)
+                _currentGoal = 0;
+        }
+
+        DrawViewState();
+    }
+
+    private void OnPlayerLost()
+    {
+        if (_distanceToPlayer <= _detectionDistance || IsInView() == false)
+        {
+
+            LostPlayer?.Invoke();
+        }
+    }
+
+    private void OnPlayerDetected()
+    {
+        if (_distanceToPlayer <= _detectionDistance || IsInView())
+        {
+            MoveToPlayer();
+            PlayerDetected?.Invoke();
+        }
+    }
+
+    private void OnNextGoal()
+    {
         if (_agent.remainingDistance < _distanceToChangeGoal)
         {
             _currentGoal++;
@@ -34,18 +95,30 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void CheckPlayerDistance()
+    private bool IsInView()
     {
-        if (Vector3.Distance(_agent.transform.position, _player.position) > _seeDistance)
+        float realAngle = Vector3.Angle(_enemyEye.forward, _player.position - _enemyEye.position);
+        RaycastHit hit;
+
+        if(Physics.Raycast(_enemyEye.transform.position, _player.position - _enemyEye.position, out hit, _viewDistance))
         {
-            _agent.speed = 4;
-            _agent.destination = _goals[_currentGoal].position;
+            if(realAngle < _viewAngle / 2f && Vector3.Distance(_enemyEye.position, _player.position) <= _viewDistance && hit.transform == _player.transform)
+                return true;
         }
-        else if (Vector3.Distance(_agent.transform.position, _player.position) <= _seeDistance)
-        {
-            _agent.speed = 6;
-            _agent.destination = _player.position;
-        }
+        return false;
+    }
+
+    private void MoveToPlayer()
+    {
+        _agent.SetDestination(_player.position);
+    }
+
+    private void DrawViewState()
+    {
+        Vector3 left = _enemyEye.position + Quaternion.Euler(new Vector3(0, _viewAngle / 2f, 0)) * (_enemyEye.forward * _viewDistance);
+        Vector3 right = _enemyEye.position + Quaternion.Euler(-new Vector3(0, _viewAngle / 2f, 0)) * (_enemyEye.forward * _viewDistance);
+        Debug.DrawLine(_enemyEye.position, left, Color.red);
+        Debug.DrawLine(_enemyEye.position, right, Color.red);
     }
 
     private void OnTriggerEnter(Collider other)
