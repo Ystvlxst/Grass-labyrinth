bc36464c637ba515fd4ab6956d4db9c23d657102
n1f1 <84967079+n1f1@users.noreply.github.com>
2022-07-29 11:03:25 +0300

    Revert "Merge branch 'v0.1' into Tatiana-ligth"
    This reverts commit b6b7f5a5e91e0d8b624e7392aa2e24231d6a58e8, reversing
    changes made to f4725feaeddae31f539d9da6395f0218a8e3da49.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,11 +4,53 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Transform[] _goals;
+    [SerializeField] private float _distanceToChangeGoal;
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _seeDistance;
     [SerializeField] private LoseCanvas _loseCanvas;
+
+    private NavMeshAgent _agent;
+    private int _currentGoal = 0;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        _agent.destination = _goals[0].position;
+    }
+
+    private void Update()
+    {
+
+        CheckPlayerDistance();
+
+        if (_agent.remainingDistance < _distanceToChangeGoal)
+        {
+            _currentGoal++;
+
+            if (_currentGoal == _goals.Length)
+                _currentGoal = 0;
+        }
+    }
+
+    private void CheckPlayerDistance()
+    {
+        if (Vector3.Distance(_agent.transform.position, _player.position) > _seeDistance)
+        {
+            _agent.speed = 4;
+            _agent.destination = _goals[_currentGoal].position;
+        }
+        else if (Vector3.Distance(_agent.transform.position, _player.position) <= _seeDistance)
+        {
+            _agent.speed = 6;
+            _agent.destination = _player.position;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out PlayerMovement player))
+        if(other.TryGetComponent(out PlayerMovement player))
             _loseCanvas.Show();
     }
 }
