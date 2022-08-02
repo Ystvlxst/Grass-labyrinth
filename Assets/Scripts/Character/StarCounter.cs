c9b34e5f524d7a7ba6d7669ea4b538f2803a939c
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-08-02 12:59:07 +0500

    Stars system

---
+++ Assets/Scripts/Character/StarCounter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCounter : MonoBehaviour
+{
+    private int _requireStarsCount;
+    private int _currentStarsCount;
+
+    private void Start()
+    {
+        _requireStarsCount = 3;
+        _currentStarsCount = 0;
+    }
+
+    private void Update()
+    {
+        CheckStarsCount();
+    }
+
+    public void FindStar()
+    {
+        _currentStarsCount++;
+    }
+
+    private void CheckStarsCount()
+    {
+        if(_currentStarsCount == _requireStarsCount)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Interactions/Star.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private StarCounter _starsCounter;
+
+    private const string _interaction = "Interaction";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out PlayerMovement player))
+        {
+            StartCoroutine(Interact());
+            _starsCounter.FindStar();
+        }
+    }
+
+    private IEnumerator Interact()
+    {
+        _animator.SetTrigger(_interaction);
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+    }
+}
