482ade463258ccaa315e96841cd0f52eeed5b193
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-08-03 12:03:14 +0500

    Change light settings

--- Assets/Scripts/Interactions/StarsSignalisation.cs
+++ Assets/Scripts/Interactions/StarsSignalisation.cs
@@ -4,13 +4,22 @@ public class StarsSignalisation : MonoBehaviour
 {
     [SerializeField] private StarCounter _starCounter;
     [SerializeField] private FoundKeyTransition[] _foundKeyTransitions;
+    [SerializeField] private ParticleSystem _findKeyEffectNoAllStars;
+    [SerializeField] private ParticleSystem _findKeyEffectAllStars;
 
     public void TookKey()
     {
         if (_starCounter.CurrentStarsCount != _starCounter.RequireStarsCount)
         {
-            foreach(var transition in _foundKeyTransitions)
+            foreach (var transition in _foundKeyTransitions)
+            {
                 transition.OnFound();
+                _findKeyEffectNoAllStars.Play();
+            }
+        }
+        else
+        {
+            _findKeyEffectAllStars.Play();
         }
     }
 }
