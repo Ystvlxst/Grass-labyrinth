05c7e1e52fa6b43536dd43489128a424a0b207e8
n1f1 <84967079+n1f1@users.noreply.github.com>
2022-08-04 17:27:23 +0300

    Maze generation asset

---
+++ Assets/MazeGeneration/MazeSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MazeSpawner : MonoBehaviour
+{
+    public Cell CellPrefab;
+    public Vector3 CellSize = new Vector3(1,1,0);
+    public Vector2 Size = new Vector2(1,1);
+    public HintRenderer HintRenderer;
+
+    public Maze maze;
+
+    private void Start()
+    {
+        MazeGenerator generator = new MazeGenerator();
+        maze = generator.GenerateMaze((int) Size.x, (int) Size.y);
+
+        for (int x = 0; x < maze.cells.GetLength(0); x++)
+        {
+            for (int y = 0; y < maze.cells.GetLength(1); y++)
+            {
+                Cell c = Instantiate(CellPrefab, transform);
+
+                c.transform.localPosition = new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z);
+                c.WallLeft.SetActive(maze.cells[x, y].WallLeft);
+                c.WallBottom.SetActive(maze.cells[x, y].WallBottom);
+            }
+        }
+
+        HintRenderer?.DrawPath();
+    }
+}
