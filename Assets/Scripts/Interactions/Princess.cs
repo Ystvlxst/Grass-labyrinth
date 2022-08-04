7d852ff75b8b5b5e1979d0ad9d97e4f7f6dfd3c3
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-08-04 19:40:43 +0500

    Princess animations

--- Assets/Scripts/Interactions/Princess.cs
+++ Assets/Scripts/Interactions/Princess.cs
@@ -1,12 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Princess : MonoBehaviour
-{ 
+{
+    [SerializeField] private Animator _animator;
+
+    private readonly  string _dance = "Dance";
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent(out Player player)) 
+        if(other.TryGetComponent(out Player player))
+        {
             player.Win();
+            _animator.SetTrigger(_dance);
+        }
     }
 }
