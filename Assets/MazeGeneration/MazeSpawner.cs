677a3df721a7a172de18f367a258831dd4f9817e
n1f1 <84967079+n1f1@users.noreply.github.com>
2022-08-04 17:39:22 +0300

    Maze editor generation

--- Assets/MazeGeneration/MazeSpawner.cs
+++ Assets/MazeGeneration/MazeSpawner.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class MazeSpawner : MonoBehaviour
 {
+#if UNITY_EDITOR
+    
     public Cell CellPrefab;
     public Vector3 CellSize = new Vector3(1,1,0);
     public Vector2 Size = new Vector2(1,1);
@@ -9,8 +12,14 @@ public class MazeSpawner : MonoBehaviour
 
     public Maze maze;
 
-    private void Start()
+    [ContextMenu("Generate")]
+    private void Generate()
     {
+        int childCount = transform.childCount;
+        
+        for (int i = 0; i < childCount; i++) 
+            DestroyImmediate(transform.GetChild(0).gameObject);
+        
         MazeGenerator generator = new MazeGenerator();
         maze = generator.GenerateMaze((int) Size.x, (int) Size.y);
 
@@ -18,14 +27,15 @@ public class MazeSpawner : MonoBehaviour
         {
             for (int y = 0; y < maze.cells.GetLength(1); y++)
             {
-                Cell c = Instantiate(CellPrefab, transform);
+                Cell cell = (Cell) PrefabUtility.InstantiatePrefab(CellPrefab, transform);
 
-                c.transform.localPosition = new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z);
-                c.WallLeft.SetActive(maze.cells[x, y].WallLeft);
-                c.WallBottom.SetActive(maze.cells[x, y].WallBottom);
+                cell.transform.localPosition = new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z);
+                cell.WallLeft.SetActive(maze.cells[x, y].WallLeft);
+                cell.WallBottom.SetActive(maze.cells[x, y].WallBottom);
             }
         }
 
         HintRenderer?.DrawPath();
     }
+#endif
 }
