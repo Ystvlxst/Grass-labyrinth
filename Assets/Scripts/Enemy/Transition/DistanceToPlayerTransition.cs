9d6d1c65d8db5b1a64be85b1a92e9444b1c5bd0c
n1f1 <84967079+n1f1@users.noreply.github.com>
2022-08-22 15:18:55 +0300

    PlayerFollow transition fix

--- Assets/Scripts/Enemy/Transition/DistanceToPlayerTransition.cs
+++ Assets/Scripts/Enemy/Transition/DistanceToPlayerTransition.cs
@@ -5,22 +5,12 @@ using Source.EnemyView;
 [RequireComponent(typeof(NavMeshAgent))]
 public class DistanceToPlayerTransition : Transition
 {
-    [SerializeField] private Enemy _enemy;
-    [SerializeField] private float _distance;
-    [SerializeField] private bool _less = true;
+    [SerializeField] private bool _notVisible = true;
     [SerializeField] private FieldOfVision _fieldOfVision;
-    
-    private Transform _player => _enemy.Player.transform;
-    private NavMeshAgent _agent;
-
-    private void Awake()
-    {
-        _agent = GetComponent<NavMeshAgent>();
-    }
 
     private void Update()
     {
-        NeedTransit = CheckDistance();
+        NeedTransit = CheckDistance() ^ _notVisible;
     }
 
     private bool CheckDistance()
