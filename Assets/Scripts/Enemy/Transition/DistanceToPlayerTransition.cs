8f412d338b9b13dae42eda60d1b6f5ea4b3f64f1
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-08-22 17:12:12 +0500

    try settings

--- Assets/Scripts/Enemy/Transition/DistanceToPlayerTransition.cs
+++ Assets/Scripts/Enemy/Transition/DistanceToPlayerTransition.cs
@@ -5,26 +5,11 @@ using Source.EnemyView;
 [RequireComponent(typeof(NavMeshAgent))]
 public class DistanceToPlayerTransition : Transition
 {
-    [SerializeField] private Enemy _enemy;
-    [SerializeField] private float _distance;
-    [SerializeField] private bool _less = true;
     [SerializeField] private FieldOfVision _fieldOfVision;
-    
-    private Transform _player => _enemy.Player.transform;
-    private NavMeshAgent _agent;
-
-    private void Awake()
-    {
-        _agent = GetComponent<NavMeshAgent>();
-    }
 
     private void Update()
     {
-        NeedTransit = CheckDistance();
-    }
-
-    private bool CheckDistance()
-    {
-        return _fieldOfVision.TryFindVisibleTarget(out Player player);
+        if (_fieldOfVision.TryFindVisibleTarget(out Player player) == true)
+            NeedTransit = true;
     }
 }
--- Assets/Scripts/Enemy/Transition/PatrolingTransition.cs
+++ Assets/Scripts/Enemy/Transition/PatrolingTransition.cs
@@ -1,19 +1,13 @@
 using UnityEngine;
+using Source.EnemyView;
 
 public class PatrolingTransition : Transition
 {
-    [SerializeField] private Enemy _enemy;
-    [SerializeField] private float _distance;
-
-    private Transform _player => _enemy.Player.transform;
+    [SerializeField] private FieldOfVision _fieldOfVision;
 
     private void Update()
     {
-        NeedTransit = CheckDistance();
-    }
-
-    private bool CheckDistance()
-    {
-        return Vector3.Distance(transform.position, _player.position) > _distance;
+        if (_fieldOfVision.TryFindVisibleTarget(out Player player) == false)
+            NeedTransit = true;
     }
 }
