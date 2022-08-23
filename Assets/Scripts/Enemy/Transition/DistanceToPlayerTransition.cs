fc48168c998f0b4be9b4ac2b0394513fa991c384
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-08-23 15:16:36 +0500

    fieldOfVision parameters changed

--- Assets/Scripts/Enemy/Transition/DistanceToPlayerTransition.cs
+++ Assets/Scripts/Enemy/Transition/DistanceToPlayerTransition.cs
@@ -1,38 +1,20 @@
 using UnityEngine;
 using UnityEngine.AI;
 using Source.EnemyView;
-using System.Collections;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class DistanceToPlayerTransition : Transition
 {
     [SerializeField] private bool _notVisible = true;
     [SerializeField] private FieldOfVision _fieldOfVision;
-    [SerializeField] private int _timeDelay = 5;
-
-    private Coroutine _coroutine;
 
     private void Update()
     {
-        if (_coroutine == null)
-            StopCoroutine(_coroutine);
-
-        _coroutine = StartCoroutine(Transit(_timeDelay));
+        NeedTransit = CheckDistance() ^ _notVisible;
     }
 
     private bool CheckDistance()
     {
         return _fieldOfVision.TryFindVisibleTarget(out Player player);
-    }
-
-    private IEnumerator Transit(int time)
-    {
-        yield return new WaitForSeconds(time);
-
-        if (_notVisible == false)
-        {
-            NeedTransit = CheckDistance() ^ _notVisible;
-            _coroutine = null;
-        }
     }
 }
